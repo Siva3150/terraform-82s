e5fbf78890369126a9f7409ef7087c6b750768e3
Siva prasad <sivaprasad3150@gmail.com>
2025-02-10 00:00:17 +0530

    user-profile

---
+++ remote-state/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.86.0"
+        }
+    }
+
+}
+
+terraform {
+  backend "s3" {
+    bucket = "82s-remote-state-siva"
+    key    = "remote-state-demo"
+    region = "us-east-1"
+    dynamodb_table = "82s-locking"
+  }
+}
+
+
+provider "aws" {
+    region = "us-east-1"
+}
